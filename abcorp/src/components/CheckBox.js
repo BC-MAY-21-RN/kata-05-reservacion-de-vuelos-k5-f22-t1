cb03ffb65b0201f403aa7712e738a87d476ab9a6
jonathan <yoni_12rrs@hotmail.com>
2022-03-08 18:46:12 -0600

    Layout RegisterScreen complete
    All the layout of the screen was added, switches were created, the state for the form was added.
    
    Co-authored-by: Jonathan <yoni_12rrs@hotmail.com>
    Co-authored-by: Cris <crprmahdz@yahoo.com>

---
+++ abcorp/src/components/CheckBox.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const CheckBox = ({text, checked}) => {
+  const [check, setCheck] = useState(false);
+
+  const handleCheck = () => {
+    setCheck(check === false ? true : false);
+  };
+
+  const ChekboxChecked = () => (
+    <TouchableOpacity onPress={handleCheck} style={styles.containerMain}>
+      <View style={styles.containerCheck}>
+        <Icon name="checkmark-outline" color={'white'} size={20} />
+      </View>
+      <Text style={styles.txtCheckbox}>{text}</Text>
+    </TouchableOpacity>
+  );
+  const ChekboxNOTChecked = () => (
+    <TouchableOpacity onPress={handleCheck} style={styles.containerMain}>
+      <View style={styles.containerNOTCheck} />
+      <Text style={styles.txtCheckbox}>{text}</Text>
+    </TouchableOpacity>
+  );
+
+  return check === true ? <ChekboxChecked /> : <ChekboxNOTChecked />;
+};
+
+const styles = StyleSheet.create({
+  containerMain: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  containerCheck: {
+    width: 25,
+    height: 25,
+    backgroundColor: '#5D60F0',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerNOTCheck: {
+    borderWidth: 1,
+    width: 25,
+    height: 25,
+  },
+  txtCheckbox: {
+    marginLeft: 10,
+    color: 'gray',
+  },
+});
--- abcorp/src/screens/RegisterScreen.js
+++ abcorp/src/screens/RegisterScreen.js
@@ -1,74 +1,191 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  Image,
 } from 'react-native';
-// import CheckBox from '@react-native-community/checkbox';
-import Icon from 'react-native-vector-icons/Ionicons';
+
+import {CheckBox} from '../components/CheckBox';
+import {useForm} from '../hooks/useForm';
 
 export const RegisterScreen = () => {
+  const {onChange, form} = useForm({
+    firstName: '',
+    email: '',
+    password: '',
+    checkedTerm: false,
+    checkedSubscribed: false,
+  });
+  const {firstName, email, password, checkedTerm, checkedSubscribed} = form;
+  const [formComplete, setFormComplete] = useState(false);
+
+  const isFormComplete = () => {
+    if (
+      firstName.length > 0 &&
+      email.length > 0 &&
+      password.length > 0 &&
+      checkedTerm
+    ) {
+      setFormComplete(true);
+    } else {
+      setFormComplete(false);
+    }
+  };
+
+  useEffect(() => {
+    isFormComplete();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [onChange]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.Title}>Sign Up</Text>
+      <Text style={styles.title}>Sign Up</Text>
       <View>
-        <Text style={styles.InptTxt}>First Name</Text>
-        <TextInput style={styles.inputs} />
-        <Text>Email</Text>
-        <TextInput style={styles.inputs} />
-        <Text>Password</Text>
-        <TextInput secureTextEntry={true} style={styles.inputs} />
+        <Text style={styles.txtInput}>First Name</Text>
+        <TextInput
+          style={[styles.inputs, firstName.length > 0 && styles.inputFill]}
+          onChangeText={value => onChange(value, 'firstName')}
+        />
+        <Text style={styles.txtInput}>Email</Text>
+        <TextInput
+          style={[styles.inputs, email.length > 0 && styles.inputFill]}
+          onChangeText={value => onChange(value, 'email')}
+        />
+        <Text style={styles.txtInput}>Password</Text>
+        <TextInput
+          secureTextEntry={true}
+          style={[styles.inputs, password.length > 0 && styles.inputFill]}
+          onChangeText={value => onChange(value, 'password')}
+        />
         <Text style={styles.txtPassword}>
           Use 8 or more characters with a mix of letters, numbers, and symbols.
         </Text>
       </View>
       <View>
-        {/* <CheckBox /> */}
-        <TouchableOpacity>
-          <View style={styles.containerCheck}>
-            <Icon name="checkmark-outline" color={'white'} size={20} />
-          </View>
-        </TouchableOpacity>
+        <CheckBox
+          text="I agree to the Terms and Privacy Policy."
+          checked={checkedTerm}
+        />
+        <CheckBox
+          text="Subscribe for select product updated."
+          checked={checkedSubscribed}
+        />
+      </View>
+      <TouchableOpacity
+        style={[styles.btnSignUp, formComplete && styles.btnSignUpOpen]}>
+        <Text style={styles.txtBtn}>Sign Up</Text>
+      </TouchableOpacity>
+      <View style={styles.containerOr}>
+        <Text style={styles.txtOr}>or</Text>
+      </View>
+      <TouchableOpacity
+        style={[
+          styles.btnSignUpGoogle,
+          formComplete && styles.btnSignUpGoogleOpen,
+        ]}>
+        <Image style={styles.imgGoogle} source={require('../img/google.png')} />
+        <Text style={styles.txtBtn}>Sign Up with Google</Text>
+      </TouchableOpacity>
+      <View style={styles.containerAccount}>
+        <Text style={styles.txtOr}>Already have an account?</Text>
+        <Text style={styles.txtLogin}> Log in</Text>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  Title: {
+  title: {
     fontSize: 22,
-    marginTop: 10,
-    marginBottom: 10,
-    // marginLeft: 10,
+    marginTop: 40,
+    marginBottom: 20,
     color: '#5D60F0',
     fontFamily: 'Impact',
+    fontWeight: 'bold',
   },
   container: {
     marginHorizontal: 20,
   },
   inputs: {
     borderColor: 'gray',
-    borderRadius: 10,
     color: 'black',
     borderWidth: 1,
     padding: 10,
-    marginTop: 10,
-  },
-  InptTxt: {
     marginTop: 15,
+    marginBottom: 15,
+  },
+  txtInput: {
+    color: 'black',
   },
   txtPassword: {
     fontSize: 13,
+    color: 'black',
+    marginBottom: 40,
   },
-  containerCheck: {
-    // borderWidth: 1,
-    width: 25,
-    height: 25,
-    backgroundColor: '#5D60F0',
+  btnSignUp: {
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    height: 50,
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    // alignItems: 'center',
+    marginTop: 20,
+  },
+  btnSignUpOpen: {
+    backgroundColor: '#5D60F0',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  btnSignUpGoogle: {
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+  btnSignUpGoogleOpen: {
+    backgroundColor: '#5D60F0',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+  txtBtn: {
+    fontSize: 21,
+    fontWeight: 'bold',
+  },
+  txtOr: {
+    color: 'rgba(0,0,0,0.3)',
+  },
+  containerOr: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  imgGoogle: {
+    height: 30,
+    width: 30,
+  },
+  containerAccount: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginVertical: 20,
+  },
+  txtLogin: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+  inputFill: {
+    borderColor: '#5D60F0',
   },
 });
